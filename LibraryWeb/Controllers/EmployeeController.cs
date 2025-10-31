850d18976cfd8a5c26864743c6f61e1f39562be7
xvysot00 <xvysot00@vutbr.cz>
2025-10-31 11:06:23 +0200

    feat(layout): update navigation links for role-based dashboard access

---
+++ LibraryWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using LibraryWeb.Data;
+using LibraryWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace LibraryWeb.Controllers
+{
+    [Authorize(Roles = "Admin,Employee")]
+    [Route("employee")]
+    public class EmployeeController : Controller
+    {
+        
+
+    }
+}
